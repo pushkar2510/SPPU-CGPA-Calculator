effa50bda831e2d1dfa01d0e2fcfeb17a028cd3e
pushkar2510 <pushkargaikwad25@gmail.com>
2025-02-12 12:04:44 +0530

    update on all 3 files designed the drop down list

--- main.js
+++ main.js
@@ -1,1 +1,17 @@
-console.log("Hello");
+/*for drop down list */
+document
+  .getElementById('target')
+  .addEventListener('change', function () {
+    'use strict';
+    var vis = document.querySelector('.vis'),   
+      target = document.getElementById(this.value);
+    if (vis !== null) {
+      vis.className = 'inv';
+    }
+    if (target !== null ) {
+      target.className = 'vis';
+    }
+});
+
+/* */
+
